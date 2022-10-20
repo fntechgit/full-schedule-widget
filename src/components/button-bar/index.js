c0508c145cbdf8a2671914d68ad7e6b190ac5d18
Santi <santipalenque@gmail.com>
2022-10-20 11:45:12 -0300

    fix current hour autoscroll

--- src/components/button-bar/index.js
+++ src/components/button-bar/index.js
@@ -38,14 +38,15 @@ const ButtonBar = ({
         <span id='timezone'>{timezoneLabel}</span>
       </Button>
 
+      {currentHour &&
       <Button
         className={`${styles.button} ${styles.liveNowBtn}`}
-        disabled={!currentHour}
         onClick={() => scroller.scrollTo('currentHour')}
       >
         <i className='fa fa-share' aria-hidden='true' />
         Jump To Live
       </Button>
+      }
 
       <div className={styles.buttonGroup}>
         <div className={styles.firstGroup}>
